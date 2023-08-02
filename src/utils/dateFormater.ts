5a0242eb4f1a8008d815386b60aed59672ca9f56
Pham Tan Dat <phamtandat172@gmail.com>
2023-08-03 05:19:14 +0700

    www

--- src/utils/dateFormater.ts
+++ src/utils/dateFormater.ts
@@ -9,3 +9,20 @@ export const formatDate = (date: any) => {
 
   return [day, month, year].join("-");
 };
+
+export const formatDate2 = (isoDate: any) => {
+  const date = new Date(isoDate);
+
+  // Lấy thông tin về giờ, phút, giây, ngày, tháng và năm
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear().toString();
+
+  // Tạo định dạng theo yêu cầu "HH:mm:ss dd-mm-yyyy"
+  const formattedDate = `${hours}:${minutes}:${seconds} ${day}-${month}-${year}`;
+
+  return formattedDate;
+};
